321ed2f577e18188f2b8e61059af393e4ef5c9ec
fabian bernhardsson <fabian.bernhardson@live.se>
2021-03-08 14:29:35 +0100

    VC

---
+++ SupLogin/ViewControllers/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SupLogin
+//
+//  Created by Fabian on 2021-02-23.
+//  Copyright © 2021 Fabian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    func setUpElements(){
+        Utilities.styleFilledButton(signUpButton)
+        Utilities.styleFilledButton(loginButton)
+    }
+
+
+}
+
